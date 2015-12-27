bf489503de844f0c175a6271a7c1420215eb09fb
Igor Zablotchi <igor.zablotchi@epfl.ch>
2015-12-27 18:17:49 +0100

    Added ascylib-style benchmark

---
+++ db/ascylib_aux/ssalloc.d
@@ -0,0 +1,3 @@
+db/ascylib_aux/ssalloc.d db/ascylib_aux/ssalloc.o: \
+ db/ascylib_aux/ssalloc.cc db/ascylib_aux/ssalloc.h \
+ db/ascylib_aux/measurements.h db/ascylib_aux/getticks.h
---
+++ db/ascylib_external/src/ssmem.d
@@ -0,0 +1,3 @@
+db/ascylib_external/src/ssmem.d db/ascylib_external/src/ssmem.o: \
+ db/ascylib_external/src/ssmem.cc \
+ db/ascylib_external/src/../include/ssmem.h
